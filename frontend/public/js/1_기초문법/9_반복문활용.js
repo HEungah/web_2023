ef6b730d160d68fd512d8d0f29df6d2afbaecf9e
504 <504@DESKTOP-PI60H60>
2023-06-23 11:56:52 +0900

    반복문 11번

--- frontend/public/js/1_기초문법/9_반복문활용.js
+++ frontend/public/js/1_기초문법/9_반복문활용.js
@@ -251,7 +251,7 @@ for(let i = hfline; i > 0; i--){
 
 
 /*
-문제10) 입력받은 줄 수 만큼 * 출력  [ ex) 5 ]
+문제10) 입력받은 줄 수 만큼 * 출력  [ ex) 7 ]
 line = 7  13(7*2-1)칸
 line = 6  11(6*2-1)칸
 line = 5  9(5*2-1)칸
@@ -269,6 +269,7 @@ line = 3  5(3*2-1)칸
 3 -> 2(i)
 
 */
+
 line = Number(prompt('[문제10]줄 수를 입력해주세요'))
 for(let i = 1; i <= line; i++){
 	if((i*2) -1 != line){
@@ -290,10 +291,102 @@ for(let i = 1; i <= line; i++){
 	}
 }
 
+/*
+문제11) 입력받은 줄 수 만큼 * 출력  [ ex) 7 ]
+line = 7  13(7*2-1)칸
+line = 6  11(6*2-1)칸
+line = 5  9(5*2-1)칸
+line = 3  5(3*2-1)칸
+*           *	line = 1 별 2개(1이상 1이하 or 13이상 13이하)
+***       ***	line = 2 별 6개(1이상 3이하 or 11이상 13이하)
+*****   *****	line = 3 별 10개(1이상 5이하 or 9이상 13이하)
+*************	line = 4 별 13개(13개)
+*****   *****	line = 5 별 10개(1이상 5이하 or 9이상 13이하)
+***       ***	line = 6 별 6개(1이상 3이하 or 11이상 13이하)
+*           *
+중간줄 판별
+7 -> 4(i)
+5 -> 3(i)
+3 -> 2(i)
+
+*/
+
+line = Number(prompt('[문제11]줄 수를 입력해주세요'))
+for(let i = 1; i <= line; i++){
+	if((i*2) -1 < line){						
+		for(let j = 0; j <(line*2) -1; j++){	
+			if(( (j >= 0) && (j < (i*2)-1) )||( (j+1 >= (line-i+1)*2 - 1) && (j <= (line*2) -2))){
+				result += '*';
+			}else{result += ' ';}
+		}
+		console.log(result);
+		result ='';
+	}else if((i*2) -1 == line){
+		for(let j = 0; j <(line*2) -1; j++){
+			result += '*';
+		}
+		console.log(result);
+		result ='';
+	}else{
+		for(let j = 0; j <(line*2) -1; j++){
+			if(( (j >= 0) && (j < (line-i+1)*2 - 1) )||( (j+1 >= i*2-1) && (j <= (line*2) -2))){
+				result += '*';
+			}else{result += ' ';}
+		}
+		console.log(result);
+		result ='';
+	}
+}
 
 
+/*
+문제12) 입력받은 줄 수 만큼 * 출력  [ ex) 7 ]
+line = 7  13(7*2-1)칸
+line = 6  11(6*2-1)칸
+line = 5  9(5*2-1)칸
+line = 3  5(3*2-1)칸
+*************	line = 1 별 13개	' ' 0개
+  *********		line = 2 별 9개	' ' 4개
+    *****		line = 3 별 5개	' ' 8개
+      *			line = 4 별 1개	' ' 12개
+    *****		line = 5 별 5개	' ' 8개
+  *********		line = 6 별 9개	' ' 4개
+*************	line = 7 별 13개 ' ' 0개
+중간줄 판별
+7 -> 4(i)
+5 -> 3(i)
+3 -> 2(i)
 
+*/
 
+line = Number(prompt('[문제12]줄 수를 입력해주세요'))
+for(let i = 1; i <= line; i++){
+	if((i*2) -1 < line){						
+		for(let j = 0; j <(line*2) -1; j++){	
+			if(( (j >= 0) && (j < (i*2)-1) )||( (j+1 >= (line-i+1)*2 - 1) && (j <= (line*2) -2))){
+				result += '*';
+			}else{result += ' ';}
+		}
+		console.log(result);
+		result ='';
+	}else if((i*2) -1 == line){
+		for(let j = 0; j <(line*2) -1; j++){
+			if(j == (i*2) -1){
+				
+			}
+		}
+		console.log(result);
+		result ='';
+	}else{
+		for(let j = 0; j <(line*2) -1; j++){
+			if(( (j >= 0) && (j < (line-i+1)*2 - 1) )||( (j+1 >= i*2-1) && (j <= (line*2) -2))){
+				result += '*';
+			}else{result += ' ';}
+		}
+		console.log(result);
+		result ='';
+	}
+}
 
 
 
