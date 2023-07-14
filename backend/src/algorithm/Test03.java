322cd0e2a2b023337f14ce35e8793a07ad871a1e
504 <504@DESKTOP-PI60H60>
2023-07-14 14:00:53 +0900

    07-14

--- backend/src/algorithm/Test03.java
+++ backend/src/algorithm/Test03.java
@@ -20,20 +20,48 @@ public class Test03 {
 		Scanner sc = new Scanner(System.in);
 		
 		String text = sc.nextLine();
-		StringBuilder sb = new StringBuilder(text);
+		// 가장 많이 쓴 알파벳을 구하는 함수를 실행
+		char resultChar = maxFrequency(text);
+		// 가장 많이 쓴 알파벳을 대문자로 바꾸어서 출력한다.
+		System.out.println(Character.toUpperCase(resultChar));
 		
-		// 입력받은 문자열을 모두 대문자로 변환
-		
-		for (int i = 0; i < text.length(); i++) {
-			if ((int)text.charAt(i) <= 122 && (int)text.charAt(i) >= 97) {
-				char chStr = (char)((int)text.charAt(i) - 32);
-				sb.setCharAt(i, chStr);
-				text = sb.toString();
-			}
-		}
-		
-		System.out.println(text);
+		sc.close();
 	}
+		
+		
+		private static char maxFrequency(String word) {
+			
+			// 입력된 문자열을 모두 소문자로 변환시켜준다.
+			String text = word.toLowerCase();
+			
+			// 알파벳배열을 생성
+			int[] aArray = new int[26];
+			
+			// 단어의 알파벳에 대응되는 알파벳배열의 인덱스의 값을 1씩증가시킴
+			for(char c : text.toCharArray()) {
+				int index = c - 'a';
+				aArray[index]++;
+			}
+			
+			
+			int manyCharacter = 0;
+			char maxCharacter = '?';
+			
+			for(int i = 0; i < aArray.length; i++) {
+				if(aArray[i] > manyCharacter) {
+					manyCharacter = aArray[i];
+					maxCharacter = (char) (i + 'a');
+				}else if(aArray[i] == manyCharacter) {
+					maxCharacter = '?';
+				}
+			}
+			
+	
+			
+			return maxCharacter;
+			
+		}
+	
 
 }
 
--- backend/src/과제/과제1/과제1.java
+++ backend/src/과제/과제1/과제1.java
@@ -5,18 +5,27 @@ public class 과제1 {
 		
 		/*
 		 * 문제 1 :	4번
-		 * 문제 2 :	2, 3, 4, 5, 6, 7번
+		 * 문제 2 :	2, 3, 6, 7번
 		 * 문제 3 :	1byte -> byte, boolean 2byte -> char, short 4byte -> int float 8byte -> double
 		 * 문제 4 :	타입 : int, double 변수이름 : age, price 리터럴 : 10, 3.14
 		 * 문제 5 :	3번
 		 * 문제 6 :	4번
-		 * 문제 7 :	6번
+		 * 문제 7 :	3, 6번
 		 * 문제 8 :	3번
 		 * 문제 9 :	1번
 		 * 문제 10 :	2번
 		 * 문제 11 :	13, 16번째 줄
 		 * 
 		 */
+		
+		String strValue = "A";
+		char charValue = 'A';
+		
+		charValue = strValue.charAt(0);
+		System.out.println(charValue);
+		
+		
+		
 
 	}
 }
